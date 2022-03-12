cf0e528dcfcb9ae0e40d4b5e45e7f9ff201278d7
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-03-12 10:11:41 +0700

    create resfull api

--- routes/products.js
+++ routes/products.js
@@ -10,4 +10,17 @@ const products = [
 router.get("/products", checkAuth,(req, res,) => {
     res.json(products);
 })
+router.get("/product/:id", checkAuth,(req, res,) => {
+    res.json(products.find(item => item.id === +req.params.id));
+})
+router.post("/products", checkAuth, (req, res) => {
+    const product = req.body;
+    res.json(product)
+})
+router.delete("/product/:id", checkAuth, (req, res) => {
+    res.json(products.filter(item => item.id !== +req.params.id))
+})
+router.put("/product/:id", checkAuth, (req, res) => {
+    res.json(products.map(item => item.id === +req.params.id ? req.body : item))
+})
 export default router;
