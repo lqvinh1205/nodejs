c7841dc7497ab466d5ab04b3f4e612ef87c5db3f
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-03-14 19:34:21 +0700

    checkout branch khác

--- controllers/products.js
+++ controllers/products.js
@@ -1,18 +1,36 @@
-export const products = [
-    {id: 1, name: "San pham A"},
-    {id: 2, name: "San pham B"},
-    {id: 3, name: "San pham C"},
-]
+// export const products = [
+//     {id: 1, name: "San pham A"},
+//     {id: 2, name: "San pham B"},
+//     {id: 3, name: "San pham C"},
+// ]
 
-export const list = (req, res,) => {
-    res.json(products);
+import mongoose from "mongoose";
+
+//1. Khoi tao modal
+const Product = mongoose.model("Product", {name: String})
+
+export const list = async (req, res,) => {
+    try {
+        const products = await Product.find()
+        res.json(products);
+    } catch (error) {
+        res.status(400).json({
+            messages: "Khong tim thay san pham"
+        })
+    }
 }
 export const read = (req, res,) => {
     res.json(products.find(item => item.id === +req.params.id));
 }
-export const create = (req, res) => {
-    const product = req.body;
-    res.json(product)
+export const create = async (req, res) => {
+    try {
+        const product = await new Product(req.body).save();
+        res.json(product)
+    } catch (error) {
+        res.status(400).json({
+            messages: "Khong the them san pham",
+        })
+    }
 }
 export const remove = (req, res) => {
     res.json(products.filter(item => item.id !== +req.params.id))
--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import mongoose from "mongoose";
+
+// import router
 import productsRouter from "../routes/products"
+
 
 const app = express();
 
+// su dung thu vien
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
+
+// route
 app.use("/api", productsRouter)
 
+// connect database
+mongoose.connect("mongodb://localhost:27017/nodejs")
+    .then(() => console.log("Kết nối db thành công"))
+    .catch((error) => console.log(error))
+ 
 
+// connection
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log("server is running port ", PORT);
