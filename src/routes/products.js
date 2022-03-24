ccc0113913629543a3c96ba408df5dc34023d33b
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-03-24 09:29:05 +0700

    .

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,9 +5,9 @@ import { checkAuth } from "../middlewares/checkAuth";
 const router = Router();
 
 router.get("/products", checkAuth, list)
-router.get("/product/:id", checkAuth, read)
+router.get("/products/:id", checkAuth, read)
 router.post("/products", checkAuth, create)
-router.delete("/product/:id", checkAuth, remove)
-router.put("/product/:id", checkAuth, update)
+router.delete("/products/:id", checkAuth, remove)
+router.put("/products/:id", checkAuth, update)
 
 export default router;
